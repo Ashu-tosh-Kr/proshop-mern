c9f0395672219b6ddcb4767f1fe2c6f1848d892f
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-07-29 03:12:19 +0530

    import assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+
 function App() {
-  return <div className="App">hello from apps</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h3>Hello from main</h3>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
