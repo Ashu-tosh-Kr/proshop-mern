dbe470a6edae8b3eaa69f374e92443e52544f185
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-09-14 05:07:42 +0530

    custom error handling

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config as dotEnvConfig } from "dotenv";
 import dbConfig from "./dbConfig.js";
 import middlewareConfig from "./middlewareConfig.js";
+import colors from "colors";
 
 const config = async (app) => {
   dotEnvConfig();
