575baffb89324bcf7c4fc85370907704e686d7aa
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-09-14 05:07:33 +0530

    products routes

--- backend/api/v1/index.js
+++ backend/api/v1/index.js
@@ -1,13 +1,10 @@
 import { Router } from "express";
 import products from "../../data/products.js";
+import productRouter from "./routes/productRoutes.js";
 
 const routes = () => {
   const router = Router();
-  router.get("/products", (req, res) => res.json(products));
-  router.get("/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-  });
+  router.use("/products", productRouter);
   return router;
 };
 export default routes;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
 import express from "express";
 import routes from "./api/v1/index.js";
 import config from "./config/index.js";
+import {
+  errorHandler,
+  notFound,
+} from "./config/middlewares/errorMiddleware.js";
 import vars from "./config/vars.js";
 
 const startServer = async () => {
   const app = express();
   await config(app);
   app.use(vars.api.prefix, routes());
-  app.listen(vars.port, () => console.log(`Listening on port ${vars.port}`));
+
+  //Custom error middleware
+  app.use(notFound);
+  app.use(errorHandler);
+
+  //start server
+  app.listen(vars.port, () =>
+    console.log(`Listening on port ${vars.port}`.yellow.bold)
+  );
 };
 startServer();
