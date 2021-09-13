2a6e5c59c4cec08f0e7733b379671fc97067d34b
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-09-14 05:06:19 +0530

    mongo setup

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -3,11 +3,16 @@ import vars from "./vars.js";
 const { connect } = mongoose;
 
 const dbConfig = async () => {
-  await connect(vars.databaseURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ignoreUndefined: true,
-  });
-  console.log("Connected to DB");
+  try {
+    await connect(vars.databaseURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ignoreUndefined: true,
+    });
+    console.log(`Connected to DB`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
 };
 export default dbConfig;
