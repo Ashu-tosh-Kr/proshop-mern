541cf78ac0984878af1abb01c089a336f09b728f
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-09-13 08:43:50 +0530

    get products

--- frontend/src/api/ProductsApi.ts
+++ frontend/src/api/ProductsApi.ts
@@ -1,10 +1,10 @@
-import axios from "axios"
 import { useQuery } from "react-query"
 import { IProduct } from "../types"
+import instance from './axiosInstance'
 
 export const useAllProducts = () => {
   const { data, isLoading, isError } = useQuery('allproducts', async():Promise<IProduct[]> => {
-    const res = await axios.get('/api/products')
+    const res = await instance.get('/api/products')
     return res.data;
   })
   const products=data as IProduct[]
@@ -13,7 +13,7 @@ export const useAllProducts = () => {
 
 export const useOneProduct = (id:string) => {
   const { data, isLoading, isError } = useQuery('product', async():Promise<IProduct> => {
-    const res = await axios.get(`/api/products/${id}`)
+    const res = await instance.get(`/api/products/${id}`)
     return res.data;
   })
   const product = data as IProduct
---
+++ frontend/src/api/axiosInstance.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+//method 1
+//NODE_ENV is set by the system itself
+// use it to change base url from absolute in dev to relative in prod
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost' : ''
+
+//method 2
+//use proxy in frontend package.json
+
+const instance = axios.create({
+  baseURL,
+});
+export default instance;
