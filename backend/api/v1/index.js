61e9b2a8fe4292ea8a899c8070b81399cac6c356
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-09-26 11:43:36 +0530

    backend: user auth

--- backend/api/v1/index.js
+++ backend/api/v1/index.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import products from "../../data/products.js";
 import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const routes = () => {
   const router = Router();
+  router.use("/users", userRouter);
   router.use("/products", productRouter);
   return router;
 };
