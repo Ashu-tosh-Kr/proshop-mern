12838dc2aa465215364f8c1d73df85ca08361b20
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-12-10 19:08:17 +0530

    deployment prep

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,19 @@ const startServer = async () => {
   await config(app);
   app.use(vars.api.prefix, routes());
 
+  //front end as static files
+  const __dirname = path.resolve();
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    );
+  } else {
+    app.get("/", (req, res) => {
+      res.send("API is running....");
+    });
+  }
+
   //Custom error middleware
   app.use(notFound);
   app.use(errorHandler);
