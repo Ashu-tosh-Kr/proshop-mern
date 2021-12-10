149042e113a4c4d1c58b5bb04cd1cb7e3677e117
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-12-10 20:15:16 +0530

    trying to deploy

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 //method 1
 //NODE_ENV is set by the system itself
 // use it to change base url from absolute in dev to relative in prod
-const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
+const baseURL =
+  process.env.REACT_APP_BASE_URL || "https://proshopped.herokuapp.com/";
 
 //method 2
 //use proxy in frontend package.json
